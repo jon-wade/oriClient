5c36770a2ed704bbfc0cd38b87933cefc076a3e5
jon-wade <jonwadeuk@gmail.com>
2020-03-10 11:16:49 +0000

    update to latest version of oriServer dependency and adjust context timeout

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/jon-wade/oriClient
 go 1.13
 
 require (
-	github.com/jon-wade/oriServer v0.0.0-20200307134406-cdef910456e7
+	github.com/jon-wade/oriServer v0.0.0-20200310111255-109269aded7b
 	google.golang.org/grpc v1.27.1
 )
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// create a context for the gRPC connection
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
 	defer cancel()
 
 	cli.Init(ctx)
